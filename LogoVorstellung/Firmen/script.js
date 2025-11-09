4d1c45ade22ff325032514ee782594602d634dd6
moexro <mo.huemmer@gmx.de>
2025-11-09 21:24:29 +0100

    script.js aktualisieren

--- LogoVorstellung/Firmen/script.js
+++ LogoVorstellung/Firmen/script.js
@@ -5,9 +5,7 @@ const firms = JSON.parse(localStorage.getItem("Firmen") || "{}");
 document.body.style.backgroundImage = `
     linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)),
     url("../Firmen/images/background_${type}.jpg")`;
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundPosition = "center";
-document.body.style.backgroundAttachment = "fixed";
+
 
 const brand = document.querySelector("#topbar .brand");
 if (brand && firms[type] && firms[type].firma) {
